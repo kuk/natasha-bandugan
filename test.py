cc84808c9226ba10a84cd0cac7c44ef1374d68ea
Alexander Kukushkin <alex@alexkuk.ru>
2024-03-23 18:46:52 +0300

    Make all test ids -1

--- test.py
+++ test.py
@@ -52,7 +52,7 @@ async def db():
 
 async def test_db_votings(db):
     voting = Voting(
-        poll_id='1',
+        poll_id='-1',
         chat_id=-1,
 
         candidate_message_id=-1,
@@ -130,7 +130,7 @@ class FakeBot(Bot):
     async def send_message(self, **kwargs):
         await self.request('sendMessage', kwargs)
         return Message(
-            message_id=1,
+            message_id=-1,
             chat=Chat(id=-1)
         )
     
@@ -148,7 +148,7 @@ class FakeBot(Bot):
         await self.request('sendPoll', kwargs)
 
         return Message(
-            message_id=1,
+            message_id=-1,
             poll=Poll(id='-1'),
             chat=Chat(id=-1),
         )
@@ -204,7 +204,7 @@ class FakeModer(Moder):
 
 class FakeBotContext(BotContext):
     def __init__(self):
-        self.bot = FakeBot('1:faketoken')
+        self.bot = FakeBot('1:token')
         self.dispatcher = Dispatcher(self.bot)
         self.db = FakeDB()
         self.moder = FakeModer()
@@ -246,7 +246,19 @@ def match_trace(trace, etalon):
 
 
 def my_chat_member_json(chat_id):
-    return '{"my_chat_member": {"chat": {"id": %d, "title": "test_bot_chat3", "type": "group", "all_members_are_administrators": true}, "from": {"id": 113947584, "is_bot": false, "first_name": "Alexander", "last_name": "Kukushkin", "username": "alexkuk", "language_code": "ru"}, "date": 1711016747, "old_chat_member": {"user": {"id": 5415060021, "is_bot": true, "first_name": "Bandugan", "username": "bandugan_bot"}, "status": "left"}, "new_chat_member": {"user": {"id": 5415060021, "is_bot": true, "first_name": "Bandugan", "username": "bandugan_bot"}, "status": "member"}}}' % chat_id
+    return '{"my_chat_member": {"chat": {"id": %d, "title": "C", "type": "group", "all_members_are_administrators": true}, "from": {"id": -1, "is_bot": false, "first_name": "A", "last_name": "K", "username": "ak", "language_code": "ru"}, "date": 1711016747, "old_chat_member": {"user": {"id": -1, "is_bot": true, "first_name": "C", "username": "c"}, "status": "left"}, "new_chat_member": {"user": {"id": -3, "is_bot": true, "first_name": "C", "username": "c"}, "status": "member"}}}' % chat_id
+
+
+def message_json(chat_id, text):
+    return '{"message": {"message_id": -1, "from": {"id": -1, "is_bot": false, "first_name": "A", "last_name": "K", "username": "ak"}, "chat": {"id": %d, "title": "C", "username": "c", "type": "supergroup"}, "date": 1711091220, "text": "%s"}}' % (chat_id, text)
+
+
+def reply_message_json(text):
+    return '{"message": {"message_id": -1, "from": {"id": -1, "is_bot": false, "first_name": "A", "last_name": "K", "username": "ak", "language_code": "ru"}, "chat": {"id": %d, "title": "C", "username": "C", "type": "supergroup"}, "date": 1658923577, "reply_to_message": {"message_id": -1, "from": {"id": -1, "is_bot": false, "first_name": "A", "last_name": "K"}, "chat": {"id": -1, "title": "C", "username": "C", "type": "supergroup"}, "date": 1658923525, "text": "..."}, "text": "%s"}}' % (CHAT_ID, text)
+
+
+def poll_answer_json(option_id):
+    return '{"poll_answer": {"poll_id": "-1", "user": {"id": -1, "is_bot": false, "first_name": "A", "last_name": "K", "username": "ak", "language_code": "ru"}, "option_ids": [%d]}}' % option_id
 
 
 async def test_leave_chat(context):
@@ -255,10 +267,6 @@ async def test_leave_chat(context):
     assert match_trace(context.bot.trace, [
         ['leaveChat', '{"chat_id": -1}']
     ])
-
-
-def message_json(chat_id, message_text):
-    return '{"message": {"message_id": 91642, "from": {"id": 694057347, "is_bot": false, "first_name": "Bulat", "last_name": "Nurgatin", "username": "nurgatin_bn"}, "chat": {"id": %d, "title": "Natural Language Processing", "username": "natural_language_processing", "type": "supergroup"}, "date": 1711091220, "text": "%s"}}' % (chat_id, message_text)
 
 
 async def test_pass(context):
@@ -270,9 +278,9 @@ async def test_pass(context):
 
 
 async def test_user_stats(context):
-    await process_update(context, message_json(CHAT_ID, 'сап'))
+    await process_update(context, message_json(CHAT_ID, '...'))
     assert context.db.user_stats == [
-        UserStats(chat_id=CHAT_ID, user_id=694057347, message_count=1)
+        UserStats(chat_id=CHAT_ID, user_id=-1, message_count=1)
     ]
 
 
@@ -280,8 +288,8 @@ async def test_use_reply(context):
     await process_update(context, message_json(CHAT_ID, '/voteban'))
     assert match_trace(context.bot.trace, [
         ['sendMessage', '{"chat_id": %d, "text": "Напиши это в реплае на спам' % CHAT_ID],
-        ['deleteMessage', '{"chat_id": %d, "message_id": 91642}' % CHAT_ID],
-        ['deleteMessage', '{"chat_id": -1, "message_id": 1}']
+        ['deleteMessage', '{"chat_id": %d, "message_id": -1}' % CHAT_ID],
+        ['deleteMessage', '{"chat_id": -1, "message_id": -1}']
     ])
     
 
@@ -290,22 +298,29 @@ async def test_auto_delete(context):
     await process_update(context, message_json(CHAT_ID, 'крипто скамерский скам'))
     assert match_trace(context.bot.trace, [
         ['sendMessage', '{"chat_id": %d, "text": "moder ban, confidence=1.0"}' % ADMIN_ID],
-        ['forwardMessage', '{"chat_id": %d, "from_chat_id": %d, "message_id": 91642}' % (ADMIN_ID, CHAT_ID)]
+        ['forwardMessage', '{"chat_id": %d, "from_chat_id": %d, "message_id": -1}' % (ADMIN_ID, CHAT_ID)]
     ])
-
-
-def reply_message_json(message_text):
-    return '{"message": {"message_id": 4, "from": {"id": 113947584, "is_bot": false, "first_name": "Alexander", "last_name": "Kukushkin", "username": "alexkuk", "language_code": "ru"}, "chat": {"id": %d, "title": "bandugan_bot_test_chat", "username": "bandugan_bot_test_chat", "type": "supergroup"}, "date": 1658923577, "reply_to_message": {"message_id": 3, "from": {"id": 5428138451, "is_bot": false, "first_name": "Alexander", "last_name": "Kukushkin"}, "chat": {"id": -1001712750774, "title": "bandugan_bot_test_chat", "username": "bandugan_bot_test_chat", "type": "supergroup"}, "date": 1658923525, "text": "abc"}, "text": "%s", "entities": [{"type": "bot_command", "offset": 0, "length": 8}]}}' % (CHAT_ID, message_text)
 
 
 async def test_start_voting(context):
     await process_update(context, reply_message_json('/voteban'))
     assert match_trace(context.bot.trace, [
-        ['getChatMember', '{"chat_id": %d, "user_id": 5428138451}' % CHAT_ID],
+        ['getChatMember', '{"chat_id": %d, "user_id": -1}' % CHAT_ID],
         ['sendPoll',  '{"chat_id": %d, "question": "Забанить' % CHAT_ID],
     ])
     assert context.db.votings == [
-        Voting(poll_id='-1', chat_id=-1, candidate_message_id=3, poll_message_id=1, start_message_id=4, starter_user_id=113947584, candidate_user_id=5428138451, ban_user_ids=[], no_ban_user_ids=[], min_votes=10)
+        Voting(
+            poll_id='-1',
+            chat_id=-1,
+            candidate_message_id=-1,
+            poll_message_id=-1,
+            start_message_id=-1,
+            starter_user_id=-1,
+            candidate_user_id=-1,
+            ban_user_ids=[],
+            no_ban_user_ids=[],
+            min_votes=10
+        )
     ]
 
 
@@ -313,20 +328,17 @@ async def test_ban_admin(context):
     context.bot.admin_chat_member = True
     await process_update(context, reply_message_json('/voteban'))
     assert match_trace(context.bot.trace, [
-        ['getChatMember', '{"chat_id": %d, "user_id": 5428138451}' % CHAT_ID],
-        ['sendMessage', '{"chat_id": %d, "text": "Alexander Kukushkin админ"' % CHAT_ID],
-        ['deleteMessage', '{"chat_id": %d, "message_id": 4}' % CHAT_ID],
-        ['deleteMessage', '{"chat_id": -1, "message_id": 1}'],
+        ['getChatMember', '{"chat_id": %d, "user_id": -1}' % CHAT_ID],
+        ['sendMessage', '{"chat_id": %d, "text": "A K админ"' % CHAT_ID],
+        ['deleteMessage', '{"chat_id": %d, "message_id": -1}' % CHAT_ID],
+        ['deleteMessage', '{"chat_id": -1, "message_id": -1}'],
     ])
 
 
-def poll_answer_json(option_id):
-    return '{"poll_answer": {"poll_id": "-1", "user": {"id": 113947584, "is_bot": false, "first_name": "Alexander", "last_name": "Kukushkin", "username": "alexkuk", "language_code": "ru"}, "option_ids": [%d]}}' % option_id
-
 INIT_VOTING = Voting(
     poll_id='-1', chat_id=-1,
-    candidate_message_id=2, poll_message_id=1, start_message_id=4,
-    starter_user_id=113947584, candidate_user_id=5428138451,
+    candidate_message_id=2, poll_message_id=1, start_message_id=3,
+    starter_user_id=-1, candidate_user_id=-2,
     ban_user_ids=[], no_ban_user_ids=[],
     min_votes=1
 )
@@ -336,26 +348,26 @@ async def test_ban_vote(context):
     context.db.votings = [INIT_VOTING]
     await process_update(context, poll_answer_json(0))
     assert match_trace(context.bot.trace, [
-        ['banChatMember', '{"chat_id": -1, "user_id": 5428138451'],
+        ['banChatMember', '{"chat_id": -1, "user_id": -2'],
         ['sendMessage', '{"chat_id": %d, "text": "voting ban"}' % ADMIN_ID],
         ['forwardMessage', '{"chat_id": %d, "from_chat_id": -1, "message_id": 2}' % ADMIN_ID],
         ['deleteMessage', '{"chat_id": -1, "message_id": 2}'],
-        ['deleteMessage', '{"chat_id": -1, "message_id": 4}'],
+        ['deleteMessage', '{"chat_id": -1, "message_id": 3}'],
         ['deleteMessage', '{"chat_id": -1, "message_id": 1}']
     ])
     voting = await context.db.get_voting(INIT_VOTING.poll_id)
-    assert voting.ban_user_ids == [113947584]
+    assert voting.ban_user_ids == [-1]
 
 
 async def test_no_ban_vote(context):
     context.db.votings = [INIT_VOTING]
     await process_update(context, poll_answer_json(1))
     assert match_trace(context.bot.trace, [
-        ['deleteMessage', '{"chat_id": -1, "message_id": 4}'],
+        ['deleteMessage', '{"chat_id": -1, "message_id": 3}'],
         ['deleteMessage', '{"chat_id": -1, "message_id": 1}']
     ])
     voting = await context.db.get_voting(INIT_VOTING.poll_id)
-    assert voting.no_ban_user_ids == [113947584]
+    assert voting.no_ban_user_ids == [-1]
 
 
 async def test_revote(context):
@@ -367,4 +379,4 @@ async def test_revote(context):
     assert context.bot.trace == []
     voting = await context.db.get_voting(INIT_VOTING.poll_id)
     assert voting.ban_user_ids == []
-    assert voting.no_ban_user_ids == [113947584]
+    assert voting.no_ban_user_ids == [-1]
